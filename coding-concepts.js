b5b736a57cd48e04743f15cba4d92639869e9a97
RoderickVongsa <roderick.vongsa@gmail.com>
2023-11-27 01:00:18 -0800

    coding-concepts complete

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,48 +8,50 @@
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
 const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+//console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Output will put indigo to the array and output "5"
+
+// b) Verify and explain: Ran the code through node in terminal and it outputed "5"
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2023"
-// console.log(cohort.length)
+//console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Output will count the number of spaces which 11
+
+// b) Verify and explain: The output was 10 and I miscounted the spaces for the output, console.log(inputwordhre.length) is the command that counts each space before the word
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+//console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This command will read the 4th word which is "o" from the Index
+// b) Verify and explain: The command outputted "o" in "Hello World!" 
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+//console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: the output will put out "Ruby" 
+// b) Verify and explain: The reason why it came out to "Ruby" because "JavaScript" is the Index which is 0
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+  //console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It will put both "saturday"&"sunday" in all caps s
+// b) Verify and explain:I tried to run the code but got an error in Terminal saying: "TypeError: weekendDays.toUpperCase is not a function"
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+ //console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: typof returns string determining its value with is expected out "number"
+// b) Verify and explain: output was number because of .length counting each value individual "subject" in the string as 4 
